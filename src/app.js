5c2b8d358e6827de8f17a9f266703b5690513c29
helenaraimundo91 <139161478+helenaraimundo91@users.noreply.github.com>
2023-10-27 20:26:31 +0000

    need to see better the css coding

--- src/app.js
+++ src/app.js
@@ -6,6 +6,32 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  document.querySelector(".card").innerHTML = generateRandomNumber();
+};
+
+let generateRandomSuit = () => {
+  let suit = ["diamond", "club", "heart", "spade"];
+  let indexsuit = Math.floor(Math.random() * suit.length);
+  return suit[indexsuit];
+};
+
+let generateRandomNumber = () => {
+  let number = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  let indexnumber = Math.floor(Math.random() * number.length);
+  return number[indexnumber];
 };
